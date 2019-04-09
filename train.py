bb868f6f3c04cfd291bd9f886363b46882047a5e
btnguyen <tuan-binh.nguyen@inria.fr>
2019-04-09 10:21:06 +0200

    log train ELBO loss & val ELBO loss

--- train.py
+++ train.py
@@ -1,9 +1,9 @@
 import functools
 import math
-
 import torch
+
 from os.path import expanduser
-from torch.optim import Adam
+from torch.optim import Adam, lr_scheduler
 from torch.utils.data import DataLoader
 from torchsummary import summary
 
@@ -11,33 +11,43 @@ from data import get_dataset
 from model import VAE, masked_mse
 
 batch_size = 48
-in_memory = True
-alpha = 10
+beta = 1  # related to \beta-VAE?
 residual = False
+in_memory = True
+return_2d = True
 
-train_dataset, test_dataset, mask = get_dataset(in_memory=in_memory)
+train_dataset, test_dataset, mask = get_dataset(in_memory=in_memory,
+                                                return_2d=return_2d)
 
 train_loader = DataLoader(train_dataset, batch_size=batch_size,
                           shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=batch_size,
                          shuffle=False)
-model = VAE()
 
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+embedding_dim = 32
+model = VAE(input_2d=return_2d, embedding_dim=embedding_dim)
+
+device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
+device_name = '%s:%i' % (device.type, device.index)
 model = model.to(device)
 mask = mask.to(device)
 
-summary(model, (1, 91, 109, 91))
+# TODO: switching from cuda:0 to cuda:1 returns error from summary
+# if return_2d:
+#     summary(model, (1, 91, 109))
+# else:
+#     summary(model, (1, 91, 109, 91))
 
 loss_function = functools.partial(masked_mse, mask=mask)
 
 optimizer = Adam(model.parameters(), lr=1e-3, amsgrad=True)
-
-n_epochs = 100
+# Adaptively change learning rate on plateau
+scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.1,
+                                           mode='min', patience=5,
+                                           threshold_mode='abs')
+n_epochs = 40
 total_loss = 0
-
 n_batch = math.ceil(len(train_dataset) / batch_size)
-
 mean = torch.zeros_like(train_dataset[0])
 # Compute mean
 if residual:
@@ -48,11 +58,17 @@ if residual:
     mean /= length
 mean = mean.to(device)
 
+# for plotting ELBO train & val loss
+list_train_elbo = []
+list_val_elbo = []
+
 for epoch in range(n_epochs):
     epoch_batch = 0
     verbose_loss = 0
     verbose_penalty = 0
     verbose_batch = 0
+    epoch_train_elbo = 0
+    epoch_val_elbo = 0
     for this_data in train_loader:
         model.train()
         model.zero_grad()
@@ -60,7 +76,7 @@ for epoch in range(n_epochs):
         this_data = this_data.to(device)
         this_data -= mean[None, ...]
         rec, penalty = model(this_data)
-        penalty *= alpha
+        penalty *= beta
         loss = loss_function(rec, this_data)
         elbo = loss + penalty
         elbo.backward()
@@ -79,7 +95,7 @@ for epoch in range(n_epochs):
                     this_test_data = this_test_data.to(device)
                     this_test_data -= mean[None, ...]
                     rec, this_val_penalty = model(this_test_data)
-                    this_val_penalty *= alpha
+                    this_val_penalty *= beta
                     this_val_loss = loss_function(rec, this_test_data)
                     val_loss += this_val_loss.item()
                     val_penalty += this_val_penalty.item()
@@ -88,18 +104,30 @@ for epoch in range(n_epochs):
             val_penalty /= val_batch
             verbose_loss /= verbose_batch
             verbose_penalty /= verbose_batch
-            print('Epoch %i, batch %i/%i,'
-                  'train_objective: %4e,'
-                  'train_penalty: %4e,'
-                  'val_objective: %4e,'
-                  'val_penalty: %4e' % (epoch, epoch_batch, n_batch,
-                                        verbose_loss, verbose_penalty,
-                                        val_loss, val_penalty))
+            train_elbo = verbose_loss + verbose_penalty
+            val_elbo = val_loss + val_penalty
+
+            print('Epoch %03i | batch %i/%i | '
+                  'train_ELBO: %4e | ' 
+                  'val_ELBO:%4e | '
+                  % (epoch, epoch_batch, n_batch, train_elbo, val_elbo))
             verbose_batch = 0
             train_loss = 0
             penalty = 0
+            epoch_train_elbo += train_elbo
+            epoch_val_elbo += val_elbo
+    epoch_train_elbo = epoch_train_elbo / n_batch
+    epoch_val_elbo = epoch_val_elbo / n_batch
+    list_train_elbo.append(epoch_train_elbo)
+    list_val_elbo.append(epoch_val_elbo)
+    scheduler.step(epoch_val_elbo)  # update learning rate
     state_dict = model.state_dict()
 
-    name = 'vae_dilated_e_%i_loss_%.4e.pkl' % (epoch, verbose_loss)
+    if return_2d:
+        name = '2D_vae_dilated_e_%03i_loss_%.4e.pkl' % (epoch, elbo)
+    else:
+        name = 'vae_dilated_e_%03i_loss_%.4e.pkl' % (epoch, elbo)
+
     torch.save((state_dict, mean),
-               expanduser('~/output/deep-fmri/%s' % name))
+               expanduser('./output/%s' % name))
+

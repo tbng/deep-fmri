2423e073c8e3020a617c6844d7152b4a19a66bff
btnguyen <tuan-binh.nguyen@inria.fr>
2019-04-09 11:18:59 +0200

    VAE works with 2D

--- model.py
+++ model.py
@@ -106,6 +106,107 @@ class Decoder(nn.Module):
         return rec
 
 
+class Encoder2d(nn.Module):
+    def __init__(self, embedding_size=128):
+        super().__init__()
+
+        self.pad = nn.ConstantPad2d((2, 3, 9, 10), 0)
+        self.conv = nn.Sequential(
+            nn.Conv2d(1, 16, 3, 2, 1),
+            nn.ReLU(),
+            nn.Conv2d(16, 16, 3, 1, 1),
+            nn.ReLU(),
+            nn.BatchNorm2d(16),
+
+            nn.Conv2d(16, 32, 3, 2, 1),
+            nn.ReLU(),
+            nn.Conv2d(32, 32, 3, 1, 1),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+
+            nn.Conv2d(32, 64, 3, 2, 1),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, 3, 1, 1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+
+            nn.Conv2d(64, 128, 3, 2, 1),
+            nn.ReLU(),
+            nn.Conv2d(128, 128, 3, 1, 1),
+            nn.ReLU(),
+            nn.BatchNorm2d(128),
+
+            nn.Conv2d(128, 256, 3, 2, 1),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, 3, 1, 1),
+            nn.ReLU(),
+            nn.BatchNorm2d(256),
+
+        )
+
+        self.dense = nn.Linear(256, embedding_size)
+        self.dense_var = nn.Linear(256, embedding_size)
+
+    def forward(self, img):
+        batch_size = img.shape[0]
+        img = self.pad(img)
+        conv_img = self.conv(img)
+        avg_channel = conv_img.view(batch_size, 256, -1).mean(dim=2)
+        # avg_channel = F.dropout(avg_channel, p=0.1)
+        mean = self.dense(avg_channel)
+        log_var = self.dense_var(avg_channel)
+        return mean, log_var
+
+
+class Decoder2d(nn.Module):
+    def __init__(self, embedding_size=128):
+        super().__init__()
+
+        self.dense = nn.Linear(embedding_size, 256)
+
+        self.deconv = nn.Sequential(
+            nn.ConvTranspose2d(256, 256, 3, 1, 1, 0),
+            nn.ReLU(),
+            nn.ConvTranspose2d(256, 128, 3, 2, 1, 1),
+            nn.ReLU(),
+            nn.BatchNorm2d(128),
+
+            nn.ConvTranspose2d(128, 128, 3, 1, 1, 0),
+            nn.ReLU(),
+            nn.ConvTranspose2d(128, 64, 3, 2, 1, 1),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+
+            nn.ConvTranspose2d(64, 64, 3, 1, 1, 0),
+            nn.ReLU(),
+            nn.ConvTranspose2d(64, 32, 3, 2, 1, 1),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+
+            nn.ConvTranspose2d(32, 32, 3, 1, 1, 0),
+            nn.ReLU(),
+            nn.ConvTranspose2d(32, 16, 3, 2, 1, 1),
+            nn.ReLU(),
+            nn.BatchNorm2d(16),
+
+            nn.ConvTranspose2d(16, 16, 3, 1, 1, 0),
+            nn.ReLU(),
+            nn.ConvTranspose2d(16, 1, 3, 2, 1, 1)
+        )
+
+    def forward(self, latent):
+        batch_size = latent.shape[0]
+        avg_channel = self.dense(latent)
+        # avg_channel = F.dropout(avg_channel, p=0.1)
+        avg_channel = avg_channel[:, :, None,
+                      None].expand(batch_size, 256, 3, 4) * 1  # why?
+        rec = self.deconv(avg_channel)
+
+        # self.pad = nn.ConstantPad3d((2, 3, 9, 10, 2, 3), 0)
+        rec = rec[:, :, 2:-3, 9:-10]  # why?
+        return rec
+
+
 class VAE(nn.Module):
     def __init__(self, embedding_dim=128, reparam=True):
         super().__init__()
